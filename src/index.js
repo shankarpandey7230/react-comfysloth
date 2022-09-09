89650113a48a4b8fbc3cb201f6d99e4443a198a2
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-09-10 03:59:07 +1000

    Auth 0 setup

--- src/index.js
+++ src/index.js
@@ -7,8 +7,18 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
+// dev-6zvbmzlo.au.auth0.com domain
+
+//client bmnV1zb4sh00BhyA7nca0mxofXqbwDZw
+
 
 ReactDOM.render(
+<Auth0Provider
+    domain="dev-6zvbmzlo.au.auth0.com"
+    clientId="bmnV1zb4sh00BhyA7nca0mxofXqbwDZw"
+    redirectUri={window.location.origin}
+    cacheLocation ='localstorage'
+  >
 <ProductsProvider>
 <FilterProvider>
 <CartProvider>
@@ -16,5 +26,6 @@ ReactDOM.render(
 </CartProvider>
 </FilterProvider>
 </ProductsProvider>
+</Auth0Provider>
 
 ,document.getElementById('root'))
